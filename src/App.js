6e294570008970de63de24720823f9f829a73a1d
ali rezaei <a.rezaei@barakat.local>
2022-02-13 17:01:58 +0330

    add navbar on ReactScrimbaExercise2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Navbar from './components/Navbar';
 
 export default class App extends React.Component {
     render() {
         return (
-        <div>
+            <div>
+                <Navbar />
+
             <h1>Hello, world!</h1>
             <p>This is a boilerplate for a React app.</p>
         </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <nav>
+                    <img src="./img/Fill 213.png"></img>
+                    <h3>MyTravelJournals</h3>
+                </nav>
+            </div>
+        );
+    }
+}
